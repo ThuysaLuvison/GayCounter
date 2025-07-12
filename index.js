e3f71339271caeed7ac1bbc044611a01f09ceaa1
ThuysaLuvison <thuysaluv@hotmail.com>
2025-07-12 15:41:46 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//Operações: + - / *
+
+//Variaveis
+
+//var num1
+//let num1 = int num1
+//const num1 = int num1 = 10
+
+//Condicionais
+
+let valor = Number(localStorage.getItem("contador")) || 0;
+const contador = document.getElementById("contador");
+const btnAumentar = document.getElementById("aumentar");
+const btnDiminuir = document.getElementById("diminuir");
+const btnZerar = document.getElementById("zerar");
+const entradaNumero = document.getElementById("entradaNumero");
+const btnDefinirNumero = document.getElementById("btnDefinirNumero");
+
+btnAumentar.addEventListener("click", () => {
+  valor++;
+  atualizarContador();
+});
+btnDiminuir.addEventListener("click", () => {
+  valor--;
+  atualizarContador();
+});
+btnZerar.addEventListener("click", () => {
+  valor = 0;
+  atualizarContador();
+});
+btnDefinirNumero.addEventListener("click", () => {
+  const valorDigitado = Number(entradaNumero.value);
+
+  if (valor === 0) {
+    valor = valorDigitado;
+    atualizarContador();
+    entradaNumero.value = "";
+  } 
+  else {
+    alert(
+      "ATENÇÃO, GAY!!! O contador não está zerado. Zere-o para definir um novo valor."
+    );
+  }
+});
+function atualizarContador() {
+  contador.textContent = valor;
+
+  contador.classList.remove("positivo", "negativo", "neutro");
+
+  if (valor > 0) {
+    contador.classList.add("positivo");
+  } else if (valor < 0) {
+    contador.classList.add("negativo");
+  } else {
+    contador.classList.add("neutro");
+  }
+
+  localStorage.setItem("contador", valor);
+
+  contador.classList.add("animar");
+  setTimeout(() => {
+    contador.classList.remove("animar");
+  }, 200);
+}
+atualizarContador();
